7cd58a0a89d00bfb8c763e38a1e6d11bc4579df2
gunwoongPark <bung1438@gmail.com>
2022-10-22 19:18:00 +0900

    feat: msw setup

---
+++ src/mocks/handlers.js
@@ -0,0 +1,16 @@
+import { rest } from "msw";
+
+const todoList = ["먹기", "자기", "놀기"];
+
+export const handlers = [
+  // 할일 목록
+  rest.get("/todos", (_, res, ctx) => {
+    return res(ctx.status(200), ctx.json(todoList));
+  }),
+
+  // 할일 추가
+  rest.post("/todos", (req, res, ctx) => {
+    todoList.push(req.body);
+    return res(ctx.status(201));
+  }),
+];
