75833eb48a4e3e68556259c32bfd216b627313d2
gunwoongPark <bung1438@gmail.com>
2022-10-22 23:19:35 +0900

    feat: todo 삭제 진행중

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useCallback, useEffect, useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { TodoListType } from "./types";
+import styled from "styled-components";
 
 function App() {
   // useState
   const [todoList, setTodoList] = useState<TodoListType[]>([]);
   const [todoInput, setTodoInput] = useState<string>("");
+  const [checkedTodoIdList, setCheckedTodoIdList] = useState<number[]>([]);
 
   // useEffect
   useEffect(() => {
@@ -42,22 +44,57 @@ function App() {
     }
   }, [todoInput, fetchTodoList]);
 
+  // delete todo
+  const onDeleteTodo = useCallback(() => {
+    if (!checkedTodoIdList.length) {
+      alert("삭제할 할 일을 선택해주세요.");
+      return;
+    }
+  }, [checkedTodoIdList.length]);
+
+  useEffect(() => {
+    console.log(checkedTodoIdList);
+  }, [checkedTodoIdList]);
+
+  const onChangeCheckbox = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      console.log("check on !!");
+    } else {
+      console.log("check off !!");
+    }
+  }, []);
+
   return (
-    <>
+    <Pub.Container>
       <ul>
         {todoList.map((todo, index) => (
-          <li key={`todo-list-item-${index}-${todo.id}`}>{todo.todo}</li>
+          <div className="todo-container">
+            <li key={`todo-list-item-${index}-${todo.id}`}>{todo.todo}</li>
+            <input type="checkbox" onChange={(e) => onChangeCheckbox(e)} />
+          </div>
         ))}
       </ul>
-      <input
-        type="text"
-        onChange={(e) => setTodoInput(e.target.value)}
-        onBlur={() => setTodoInput((prevInputState) => prevInputState.trim())}
-        value={todoInput}
-      />
-      <button onClick={() => onSaveTodo()}>submit</button>
-    </>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setTodoInput(e.target.value)}
+          onBlur={() => setTodoInput((prevInputState) => prevInputState.trim())}
+          value={todoInput}
+        />
+        <button onClick={() => onSaveTodo()}>save</button>
+      </div>
+      <button onClick={() => onDeleteTodo()}>delete</button>
+    </Pub.Container>
   );
 }
 
 export default App;
+
+const Pub = {
+  Container: styled.div`
+    .todo-container {
+      display: flex;
+      align-items: center;
+    }
+  `,
+};
